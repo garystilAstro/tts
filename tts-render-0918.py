974dde693c6549de7fd6c5804b3ca1e51e52a586
garystilAstro <161110593+garystilAstro@users.noreply.github.com>
2024-09-18 21:49:09 -0600

    Update tts-render-0918.py

--- tts-render-0918.py
+++ tts-render-0918.py
@@ -28,7 +28,7 @@ speech_file_path.parent.mkdir(parents=True, exist_ok=True)
 
 @app.route('/')
 def index():
-    return render_template('templates/tts-render-0918.html')
+    return render_template('tts-render-0918.html')
 
 @app.route('/generate-speech', methods=['POST'])
 def generate_speech():
